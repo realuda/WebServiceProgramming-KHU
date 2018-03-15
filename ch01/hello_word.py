a901c9caff99ada3a7c9174f6d706903b08c6521
Remakaw <30518853+realuda@users.noreply.github.com>
2018-03-15 19:34:15 +0900

    Add files via upload

---
+++ ch01/hello_word.py
@@ -0,0 +1,9 @@
+#web service programming python practice
+print("Hello, World")
+print("my name is Hyeongseok Kim")
+
+birthYear = 1996
+age = 23
+
+for i in range(1,age+1):
+    print("%d년 내 나이는 %d살" %(birthYear-1+i, i))
